c1f2af48784771a6c668820868f103762b0f20cf
Vipin Kumar <vipin.kumar@concirrus.com>
2018-11-04 21:32:45 +0530

    Added project comments

--- src/main/java/com/portextractor/EurekaPortExtractorApplication.java
+++ src/main/java/com/portextractor/EurekaPortExtractorApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+/*
+ * Port Extracter from Service Regsitry
+ * 
+ */
 @EnableEurekaClient
 @SpringBootApplication
 public class EurekaPortExtractorApplication {
